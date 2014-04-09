d29109e6ceafeb30ecc34fda6cc403b3fca64655
TheoRettisch <mark.burbach@gmail.com>
2014-04-09 02:33:53 +0200

    Updated Block Explorer
    Added new official Block Explorer

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -34,9 +34,9 @@ nets = dict(
         BLOCK_PERIOD=60, # s
         SYMBOL='HIRO',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Hirocoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Hirocoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.hirocoin'), 'hirocoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://coyn.org/hirocoin/explorer/block/index.php?hash=',
-        ADDRESS_EXPLORER_URL_PREFIX='http://coyn.org/hirocoin/explorer/address/?address=',
-        TX_EXPLORER_URL_PREFIX='http://coyn.org/hirocoin/explorer/tx/?txid=',
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.hirocoin.org/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.hirocoin.org/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.hirocoin.org/tx/',
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1), 
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.001e8,
